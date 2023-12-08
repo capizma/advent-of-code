e6482e83fb1584d25679020f61332888f508de10
Louis Ducasse <louis.ducasse2022@outlook.com>
2023-12-08 01:07:59 +0000

    days 4&6

---
+++ day4/day4.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <iomanip> 
+#include <chrono>
+using namespace std;
+
+int main() {
+	string ln;
+	string line;
+	ifstream file("input.txt");
+	int total = 0;
+	int power_score = 0;
+	vector<string> cards = {};
+	vector<int> powerslist = {};
+	int copieslist[203][2];
+	int secondtotal = 0;
+
+	if (file.is_open()) {
+		while (getline(file, ln)) {
+			cards.push_back(ln);
+		}
+		int original_size = cards.size();
+
+		for (int card = 0; card < original_size; card++) {
+			line = cards[card];
+			size_t split1 = line.find(':') + 2;
+			size_t split2 = line.find('|');
+			string winning = ' ' + line.substr(split1, split2 - split1);
+			string numbers = ' ' + line.substr(split2, line.size() - split2) + ' ';
+			string digit = " ";
+			power_score = 0;
+			bool found = false;
+			for (int i = 0; i < numbers.size(); i++) {
+				if (numbers[i] >= 48 && numbers[i] <= 57) {
+					digit.push_back(numbers[i]);
+				}
+				else {
+					if (digit != " ") {
+						int it;
+						digit.push_back(' ');
+						it = winning.find(digit);
+						if (it != -1) {
+							power_score += 1;
+							found = true;
+						}
+					}
+					digit = " ";
+				}
+			}
+			powerslist.push_back(power_score);
+			total += floor(pow(2, power_score) / 2);
+		}
+		file.close();
+	}
+
+	for (int i = 0; i < powerslist.size(); i++) {
+		copieslist[i][0] = powerslist[i];
+		copieslist[i][1] = 1;
+	}
+
+	for (int i = 0; i < powerslist.size(); i++) {
+		for (int j = i + 1; j < i + copieslist[i][0] + 1; j++) {
+			int quant = 1 * copieslist[i][1];
+			copieslist[j][1] += quant;
+			secondtotal += quant;
+		}
+	}
+
+	secondtotal += powerslist.size();
+
+	std::cout << "Part 1: " << total << '\n';
+	std::cout << "Part 2: " << secondtotal << '\n';
+
+	system("pause");
+	return 0;
+}
---
+++ day6/day 6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int64_t gametime = 0;
+int64_t record = 0;
+int total = 1;
+int64_t secondtotal = 0;
+vector<int64_t> ls = { 59,543,68,1020,82,1664,74,1022,59688274,543102016641022};
+
+int main()
+{
+	for (int64_t c = 0; c < ls.size() - 1; c += 2) {
+		int64_t pos = 0;
+		gametime = ls[c];
+		for (int i = 1; i < gametime; i++) {
+			if (((gametime - i) * i) > ls[c + 1]) {
+				pos = i;
+				break;
+			}
+		}
+		int64_t games = 0;
+		games = (gametime + 1) - (pos * 2);
+
+		if (c != ls.size() - 2) {
+			total*= games;
+		}
+		else {
+			secondtotal = games;
+		}
+	}
+	std::cout << "Part 1: " << total << '\n';
+	std::cout << "Part 2: " << secondtotal << '\n';
+
+	return 0;
+}
